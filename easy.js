99fa9611b3004288fa96ac3ba5be34c64049ee54
yasminejim <94918510+yasminejim@users.noreply.github.com>
2022-01-20 12:10:23 -0500

    first commit

---
+++ easy.js
@@ -0,0 +1,13 @@
+
+
+
+var run = exercise('running');
+
+console.log(run()); // prints "Today's exercise: running"
+
+var swim = exercise('swimming');
+
+console.log(swim()); // prints "Today's exercise: swimming"
+
+
+
